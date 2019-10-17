c2f03e561ef0dfc66e19bde5a8907c47f921f3d8
jungsw <jungsw586@gmail.com>
2019-10-17 13:26:21 +0900

    refresh

--- src/Component_main/Nav/NavInputArea.js
+++ src/Component_main/Nav/NavInputArea.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 
 class NavInputArea extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchKeyword : '',
+        }
+    }
+
+    handlerOnChanged = (e) => {
+        this.setState({
+            searchKeyword: e.target.value,
+        })
+    }
+
     render() {
+        console.log(this.state.searchKeyword)
         return (
             <div className="nav-input-area">
-                <input className="nav-input-Box" type="text" />
+                <input
+                    className="nav-input-Box"
+                    type="text"
+                    name="searchKeyword"
+                    onClick={this.handlerOnChanged}
+                />
                 <div className="filteredBox"></div>
             </div>        
         );
